80eaf5fa75533f3601bad48ac278a9c5506ff37b
Hongwei <gmhwxi@gmail.com>
2023-01-15 07:54:39 -0500

    Updating: very very minorly

--- CODE/Python/mylist.py
+++ CODE/Python/mylist.py
@@ -1,7 +1,23 @@
 class mylist(object):
     def get_ctag():
         return self.ctag
+    def __iter__(self):
+        return mylist_iter(self)
 # end-of-class(mylist)
+
+class mylist_iter:
+    def __init__(self, xs):
+        self.xs = xs
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if (self.xs.ctag==0):
+            raise StopIteration
+        else:
+            head = self.xs.head
+            self.xs = self.xs.tail
+            return head
+    # end-of-[__next__]
 
 class mylist_nil(mylist):
     def __init__(self):
@@ -21,11 +37,10 @@ class mylist_cons(mylist):
         return self.tail
 # end-of-class(mylist_cons)
 
-def mylist_make_list(xs):
-    res = mylist_nil()
-    for x1 in xs:
-        res = mylist_cons(x1, res)
-    return mylist_reverse(res)
+def mylist_nilq(xs):
+    return (xs.ctag == 0)
+def mylist_consq(xs):
+    return (xs.ctag != 0)
 
 def mylist_print(xs):
     sep = ";"
@@ -35,6 +50,12 @@ def mylist_print(xs):
             print(xs.head,end=''); auxlst(xs.tail, n0+1)
     print("(", end=''); auxlst(xs, 0); print(")", end='')
 # end-of-[mylist_print]
+
+def mylist_make_list(xs):
+    res = mylist_nil()
+    for x1 in xs:
+        res = mylist_cons(x1, res)
+    return mylist_reverse(res)
 
 def mylist_length(xs):
     return (0 if(xs.ctag==0) else 1 + mylist_length(xs.tail))
@@ -52,6 +73,13 @@ def mylist_rappend(xs, ys):
     return (ys if(xs.ctag==0) else mylist_rappend(xs.tail, mylist_cons(xs.head, ys)))
 # end-of-[mylist_rappend]
 
+def mylist_forall(xs, test):
+    return True if (xs.ctag == 0) else (mylist_forall(xs.tail, test) if test(xs.head) else False)
+
+def mylist_foreach(xs, work):
+    def sqn2(skipped, result): return result
+    mylist_forall(xs, lambda x: sqn2(work(x), True)); return None
+
 #
 # Here is some testing code:
 #
@@ -62,3 +90,9 @@ xsys = mylist_append(xs, ys)
 print("xsys = ", end=''); mylist_print(xsys); print()
 sysx = mylist_reverse(xsys)
 print("sysx = ", end=''); mylist_print(sysx); print()
+
+i0 = 0
+for x0 in xs:
+    print("xs[",i0,"] = ",sep='',end=''); print(x0)
+    i0 = i0 + 1
+mylist_foreach(xs, lambda x: print(x))
---
+++ assigns/00/assign00.sml
@@ -0,0 +1,58 @@
+(* ****** ****** *)
+(*
+Assign00: Warmup!
+*)
+(* ****** ****** *)
+
+(*
+fun fact(x: int): int =
+  if x > 0 then x * fact(x-1) else 1
+*)
+
+(*
+Assign00-01: 10 points
+Please find the first integer N such that the
+evaluation of fact(N) in SML/NJ yields an Overflow
+exception.
+*)
+
+(* ****** ****** *)
+
+(*
+Assign00-02: 10 points
+Please implement a function that tests whether a
+given natural number is a prime:
+fun isPrime(n0: int): bool
+*)
+
+(* ****** ****** *)
+
+(*
+Assign00-03: 10 points
+Please implement a function that converts a given
+integer to a string that represents the integer:
+fun int2str(i0: int): string
+*)
+
+(* ****** ****** *)
+
+(*
+Assign00-04: 10 points
+Please implement a function that converts a given
+string to an integer:
+fun str2int(cs: string): int
+In particular, it is expected that str2int(int2str(x)) = x
+*)
+
+(* ****** ****** *)
+
+(*
+Assign00-05: 10 points
+Please implement a function that returns the reverse of
+a given string:
+fun stringrev(cs: string): string
+*)
+
+(* ****** ****** *)
+
+(* end of [CS320-2023-Spring-assign00.sml] *)
