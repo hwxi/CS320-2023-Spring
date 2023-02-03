d4de1f69ad32504508de76b537cc7e338d79be48
Hongwei <gmhwxi@gmail.com>
2023-02-02 21:58:55 -0500

    Updating: very very minorly

--- assigns/02/MySolution/assign02_01.py
+++ assigns/02/MySolution/assign02_01.py
@@ -12,24 +12,3 @@ print("[import ./../assign02.py] is done!")
 # mylist_reverse (see list_reverse in assign02.sml)
 #
 ####################################################
-
-def mylist_append(xs, ys):
-    if (xs.ctag==0):
-        return ys
-    else:
-        return mylist_cons(xs.cons1, mylist_append(xs.cons2, ys))
-    # end-of-[if-then-else]
-
-####################################################
-
-def mylist_reverse(xs):
-    return mylist_rappend(xs, mylist_nil())
-
-def mylist_rappend(xs, ys):
-    if (xs.ctag==0):
-        return ys
-    else:
-        return mylist_rappend(xs.cons2, mylist_cons(xs.cons1, ys))
-    # end-of-[if-then-else]
-
-####################################################
