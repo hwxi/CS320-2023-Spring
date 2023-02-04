329d21a4afc7c3d7f10e814c42c627f844b8999c
Hongwei <gmhwxi@gmail.com>
2023-02-04 17:04:58 -0500

    Updating: very very minorly

--- assigns/02/assign02.py
+++ assigns/02/assign02.py
@@ -3,6 +3,10 @@
 # Assign02: FP in Python
 # *)
 ####################################################
+# see: MySolution/assign02_01.py: 20 points
+# see: MySolution/assign02_02.py: 10 points
+# see: MySolution/assign02_03.py: 20 points
+####################################################
 
 # datatype 'a list =
 # nil | cons of ('a * 'a list)
