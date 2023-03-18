8f9359add5b099f2cc4d7af16dc25102bc85ecf8
Hongwei <gmhwxi@gmail.com>
2023-03-18 18:49:22 -0400

    Updating: very very minorly

---
+++ assigns/05/Testing/Python/assign05_03_test.py
@@ -0,0 +1,35 @@
+######################################################
+import sys
+sys.path.append('./../../MySolution/Python')
+from assign05_03 import *
+######################################################
+balloons = \
+    load_color_image\
+    ("./../../MySolution/Python/INPUT/balloons.png")
+######################################################
+ncol = 1
+balloons_1 = image_seam_carving_color(balloons, ncol)
+######################################################
+ptotal = 0
+def work_func(clr):
+    global ptotal
+    ptotal = ptotal + sum(clr)
+imgvec.image_foreach\
+    (balloons_1, lambda clr: work_func(clr))
+print("ptotal = ", ptotal)
+assert (ptotal == 58811238)
+######################################################
+ncol = 2
+balloons_2 = image_seam_carving_color(balloons, ncol)
+######################################################
+ptotal = 0
+def work_func(clr):
+    global ptotal
+    ptotal = ptotal + sum(clr)
+imgvec.image_foreach\
+    (balloons_2, lambda clr: work_func(clr))
+print("ptotal = ", ptotal)
+assert (ptotal == 58699911)
+######################################################
+
+#### end of [CS320-2023-Spring-assign05_03_test.py] ####
