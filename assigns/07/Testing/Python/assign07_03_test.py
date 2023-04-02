ecbc79423e2bc1821ff90cc5c42a31462aef4294
Hongwei <gmhwxi@gmail.com>
2023-04-01 21:44:11 -0400

    Updating: very very minorly

--- assigns/07/Testing/Python/assign07_03_test.py
+++ assigns/07/Testing/Python/assign07_03_test.py
@@ -7,9 +7,19 @@ from assign07_03 import *
 #     (doublet_stream_from('water'), \
 #      lambda i, x: i < 1000 and not(print(i,":",x)))
 ######################################################
-assert(doublet_bfs_test('never', 'again'))
-assert(doublet_bfs_test('flour', 'bread'))
-assert(not(doublet_bfs_test('hello', 'world')))
+test1 = \
+    doublet_bfs_test('never', 'again')
+print("test1 = ", test1)
+assert test1
+test2 = \
+    doublet_bfs_test('flour', 'bread')
+print("test2 = ", test2)
+assert test2
+test3 = \
+    doublet_bfs_test('hello', 'world')
+print("test3 = ", test3)
+assert test3
+assert not(doublet_bfs_test('hello', 'xerox'))
 ######################################################
 print("Assign07-03-test passed!")
 ######################################################
