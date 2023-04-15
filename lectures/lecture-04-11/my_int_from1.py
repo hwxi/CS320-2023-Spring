8926a8767f168b8997c68f8b16ff96fc8fd4eead
Hongwei <gmhwxi@gmail.com>
2023-04-15 07:05:09 -0400

    Updating: very very minorly

--- lectures/lecture-04-11/my_int_from1.py
+++ lectures/lecture-04-11/my_int_from1.py
@@ -2,8 +2,11 @@
 import sys
 ################################################
 def my_int_from1(start):
+    # print("my_int_from1: while-bef")
     while True:
+        # print("my_int_from1: yield-bef")
         yield start
+        # print("my_int_from1: yield-aft")
         start = start + 1
 ################################################    
 for i in my_int_from1(0):
@@ -12,4 +15,5 @@ for i in my_int_from1(0):
     else:
         print("i =", i)
 ################################################
+
 
